b107aac747268a9664638f4fb2c71d8b8bc91084
AeLinLee <dldofls666@gmail.com>
2024-09-24 12:05:48 +0900

    exam01_데이터 종류

---
+++ src/exam01_데이터종류/PDT02_실수형2_범위.java
@@ -0,0 +1,18 @@
+package exam01_데이터종류;
+
+public class PDT02_실수형2_범위 {
+
+	public static void main(String[] args) {
+		
+      //실수형 범위
+		
+		System.out.println("float 저장범위");
+		System.out.println(Float.MAX_VALUE);  //3.4028235E38
+		System.out.println(Float.MIN_VALUE);  //1.4E-45
+		
+		System.out.println("double 저장범위");
+		System.out.println(Double.MAX_VALUE);  //1.7976931348623157E308
+		System.out.println(Double.MIN_VALUE);  //4.9E-324
+	}
+
+}
